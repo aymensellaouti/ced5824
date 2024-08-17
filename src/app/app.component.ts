425a6e79ff16d0ce85e52f9c193859ef2e97e778
aymensellaouti <aymen.sellaouti@gmail.com>
2024-08-17 12:12:38 +0100

    config ngsw to handle api caching with datagroup and add push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { SwPush, SwUpdate } from '@angular/service-worker';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { APP_CONSTS } from './config/constantes.config';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,9 @@ import { NgxUiLoaderService } from 'ngx-ui-loader';
 export class AppComponent {
   title = 'ced5824';
   router = inject(Router);
+  http = inject(HttpClient);
+  swUpdate = inject(SwUpdate);
+  swPush = inject(SwPush);
   ngxService = inject(NgxUiLoaderService);
   constructor() {
     this.router.events.subscribe((event) => {
@@ -23,5 +29,38 @@ export class AppComponent {
         this.ngxService.stop();
       }
     });
+    this.swUpdate.versionUpdates.subscribe((version) => {
+      console.log('Checking Version Update');
+
+      if (version.type === 'VERSION_READY') {
+        if (
+          confirm(
+            `Une nouvelle version de l'application est disponionible, vouslez vous recharger la page ?`
+          )
+        ) {
+          window.location.reload();
+        }
+      }
+    });
+  }
+
+  subscribeToNotifications() {
+    console.log('In subscribeToNotifications');
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: APP_CONSTS.vapidKeys.publicKey,
+      })
+      .then((sub: PushSubscription) => {
+        // Faites ce que vous voulez avec cet objet
+        console.log({ sub });
+        this.http
+          .post('https://nest-push-ghxv.vercel.app/notifications', sub)
+          .subscribe((data) => console.log({ data: data }));
+      })
+      .catch((err) => {
+        //en cas de refus});
+        console.log('pbm');
+        console.log({ err });
+      });
   }
 }
--- src/app/config/constantes.config.ts
+++ src/app/config/constantes.config.ts
@@ -1,5 +1,10 @@
 export const APP_CONSTS = {
   defaultImage: 'tim_logo.png',
   tokenKey: 'token',
-  connectedUser: 'connectedUser'
+  connectedUser: 'connectedUser',
+  vapidKeys: {
+    publicKey:
+      'BCucKI9WG2aEbOhigJ6y_3Z28GIe0jp9QjaZtXXUABjGoUMup6IoYUrTSMh72MxP3t6eVoV6cZ1doEGMm9cRJ2Q',
+    privateKey: '*',
+  },
 };
