0281bbf1ff5cb5dfe0343eee92d09bc745aa77ca
aymensellaouti <aymen.sellaouti@gmail.com>
2024-08-20 13:00:11 +0100

    link deploy

--- src/app/config/constantes.config.ts
+++ src/app/config/constantes.config.ts
@@ -2,5 +2,6 @@ export const APP_CONSTS = {
   defaultImage: 'tim_logo.png',
   tokenKey: 'token',
   connectedUser: 'connectedUser',
-  addForm: 'addForm'
+  addForm: 'addForm',
+  deployLink: 'https://ced5824.vercel.app/',
 };
