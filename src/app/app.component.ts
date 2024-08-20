b07486884b1ea5cb71d9c9dba1adc763e2a8bcd4
aymensellaouti <aymen.sellaouti@gmail.com>
2024-08-20 15:12:09 +0100

    push notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Store } from '@ngrx/store';
 import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { AppState } from './store/reducer';
 import { initAppAction } from './store/actions';
+import { SwPush, SwUpdate } from '@angular/service-worker';
+import { APP_CONSTS } from './config/constantes.config';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,8 +16,11 @@ import { initAppAction } from './store/actions';
 export class AppComponent {
   title = 'ced5824 ';
   router = inject(Router);
+  http = inject(HttpClient);
   ngxService = inject(NgxUiLoaderService);
   store = inject(Store<AppState>);
+  swUpdate = inject(SwUpdate);
+  swPush = inject(SwPush);
   constructor() {
     // this.router.events.subscribe((event) => {
     //   if (event instanceof NavigationStart) {
@@ -28,5 +34,41 @@ export class AppComponent {
     //   }
     // });
     this.store.dispatch(initAppAction());
+
+    this.swUpdate.versionUpdates.subscribe((version) => {
+      console.log('Checking Version Update');
+
+      if (version.type === 'VERSION_READY') {
+        console.log('VERSION READY');
+
+        if (
+          confirm(
+            `Une nouvelle version de l'application est disponionible, vouslez vous recharger la page ?`
+          )
+        ) {
+          window.location.reload();
+        }
+      }
+    });
+  }
+
+  subscribeToNotifications() {
+    console.log('In subscribeToNotifications');
+    this.swPush
+      .requestSubscription({
+        serverPublicKey: APP_CONSTS.vapidKeys.publicKey,
+      })
+      .then((sub: PushSubscription) => {
+        // Faites ce que vous voulez avec cet objet
+        console.log({ sub });
+        this.http
+          .post('https://nest-push-ghxv.vercel.app/notifications', sub)
+          .subscribe((data) => console.log({ data: data }));
+      })
+      .catch((err) => {
+        //en cas de refus});
+        console.log('pbm');
+        console.log({ err });
+      });
   }
 }
