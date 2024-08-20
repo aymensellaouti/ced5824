bab8f57122256408d45672e8c6c16c8f5f43d81b
aymensellaouti <aymen.sellaouti@gmail.com>
2024-08-20 12:48:28 +0100

    mise en place du pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import { StoreModule } from '@ngrx/store';
 import { appReducer } from './store/reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 @NgModule({
@@ -84,6 +85,12 @@ import { EffectsModule } from '@ngrx/effects';
     StoreModule.forRoot({ ux: appReducer }, {}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     EffectsModule.forRoot([]),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {
